4492287c22b76986d82aca4b44d44444eedb7f12
pratyush1279 <134144564+pratyush1279@users.noreply.github.com>
2024-01-17 13:48:31 +0530

    Create escape sequence.c
    escape sequence

---
+++ escape sequence.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main ()
+{
+    printf("Hello world");
+    printf("\nPratyush can do everything");
+    printf("\nPratyush is a hard working person");
+    printf("\nHe is a front end engineer");
+    
+    return 0;
+}
